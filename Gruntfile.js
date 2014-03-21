a986650d49864af4ea463559e8c112ef850c12aa
silverwind <me@silverwind.io>
2014-03-21 19:35:42 +0100

    Basic SASS infrastructure #127

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+    "use strict";
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    "build/github-dark.css": "github-dark.sass"
+                }
+            }
+        },
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: "build/",
+                dest: "build/",
+                src: "github-dark.css",
+                ext: ".min.css"
+            }
+        }
+    });
+
+    grunt.registerTask("compile", ["sass"]);
+    grunt.registerTask("default", ["sass", "cssmin"]);
+
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-sass");
+};
