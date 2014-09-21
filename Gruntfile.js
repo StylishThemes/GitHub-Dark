24c5f83ef4d11b666ded74cc0742e539d8f5575b
silverwind <me@silverwind.io>
2014-09-21 23:10:52 +0200

    Build: Fix config.json parsing, #181

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,93 +3,89 @@ module.exports = function (grunt) {
 
     var config, getTheme;
 
-    if (process.argv[2] !== 'minify') {
-        try {
-            config = require("fs").readFileSync('build.json');
-        } catch (err) {
-            if (err.code === 'ENOENT') {
-                console.info('build.json not found - using defaults');
-                config = {
-                    'theme'  : 'GitHub-Dark',
-                    'color'  : '#008080',
-                    'image'  : 'url(https://raw.githubusercontent.com/StylishThemes/GitHub-Dark/master/images/backgrounds/bg-tile1.png)',
-                    'tiled'  : true,
-                    'attach' : 'scroll',
-                    'tab'    : 4,
-                    'chrome' : false
-                };
-            }
-        }
-
-        getTheme = function () {
-            return config.theme.toLowerCase().replace(/\s+/g, '-');
+    try {
+        config = grunt.file.readJSON('build.json');
+    } catch (err) {
+        console.info('build.json not found - using defaults');
+        config = {
+            'theme'  : 'GitHub-Dark',
+            'color'  : '#008080',
+            'image'  : 'url(https://raw.githubusercontent.com/StylishThemes/GitHub-Dark/master/images/backgrounds/bg-tile1.png)',
+            'tiled'  : true,
+            'attach' : 'scroll',
+            'tab'    : 4,
+            'chrome' : false
         };
+    }
 
-        // ** set up build options **
-        // do we need a check to see if the theme file exists?
-        config.themeFile = 'themes/pygments-' + getTheme() + '.min.css';
-        // build file name
-        config.buildFile = 'github-dark-' + getTheme() + '-' + config.color.replace(/[^\d\w]/g, '') + '.build.css';
-        // background options
-        config.bgOptions = config.tiled ?
-            'background-repeat: repeat !important; background-size: auto !important; background-position: left top !important;' :
-            'background-repeat: no-repeat !important; background-size: cover !important; background-position: center top !important;';
-        config.bgOptions2 = config.attach.toLowerCase() === 'scroll' ?
-            'background-attachment: scroll !important;' :
-            'background-attachment: fixed !important;';
+    getTheme = function () {
+        return config.theme.toLowerCase().replace(/\s+/g, '-');
+    };
 
-        // Don't include closing bracket for a chrome build
-        config.newTheme = '<%= grunt.file.read("' + config.themeFile + '") %>' + (config.chrome ? '' : '\n}');
+    // ** set up build options **
+    // do we need a check to see if the theme file exists?
+    config.themeFile = 'themes/pygments-' + getTheme() + '.min.css';
+    // build file name
+    config.buildFile = 'github-dark-' + getTheme() + '-' + config.color.replace(/[^\d\w]/g, '') + '.build.css';
+    // background options
+    config.bgOptions = config.tiled ?
+        'background-repeat: repeat !important; background-size: auto !important; background-position: left top !important;' :
+        'background-repeat: no-repeat !important; background-size: cover !important; background-position: center top !important;';
+    config.bgOptions2 = config.attach.toLowerCase() === 'scroll' ?
+        'background-attachment: scroll !important;' :
+        'background-attachment: fixed !important;';
 
-        // custom build
-        config.replacements = [{
-            pattern: '/*[[bg-choice]]*/ url(https://stylishthemes.github.io/GitHub-Dark/images/backgrounds/bg-tile1.png)',
-            replacement: config.image
-        },{
-            pattern: '/*[[bg-options]]*/',
-            replacement: config.bgOptions
-        },{
-            pattern: '/*[[bg-options2]]*/',
-            replacement: config.bgOptions2
-        },{
-            pattern: /\/\*\[\[base-color\]\]\*\/ #4183C4/g,
-            replacement: config.color
-        },{
-            pattern: /\/\*\[\[tab-size\]\]\*\/ 4/g,
-            replacement: config.tab
-        },{
-            // remove default syntax theme AND closing bracket
-            pattern: /\s+\/\* GitHub-Dark syntax highlighting(.*(\n|\r))+}$/m,
+    // Don't include closing bracket for a chrome build
+    config.newTheme = '<%= grunt.file.read("' + config.themeFile + '") %>' + (config.chrome ? '' : '\n}');
+
+    // custom build
+    config.replacements = [{
+        pattern: '/*[[bg-choice]]*/ url(https://stylishthemes.github.io/GitHub-Dark/images/backgrounds/bg-tile1.png)',
+        replacement: config.image
+    },{
+        pattern: '/*[[bg-options]]*/',
+        replacement: config.bgOptions
+    },{
+        pattern: '/*[[bg-options2]]*/',
+        replacement: config.bgOptions2
+    },{
+        pattern: /\/\*\[\[base-color\]\]\*\/ #4183C4/g,
+        replacement: config.color
+    },{
+        pattern: /\/\*\[\[tab-size\]\]\*\/ 4/g,
+        replacement: config.tab
+    },{
+        // remove default syntax theme AND closing bracket
+        pattern: /\s+\/\* GitHub-Dark syntax highlighting(.*(\n|\r))+}$/m,
+        replacement: ''
+    },{
+        pattern: '/*[[pygments-theme]]*/',
+        // add selected theme + closing bracket (if not chrome)
+        replacement: config.newTheme
+    }];
+
+    // userstyles.org - remove defaults & leave placeholders
+    config.replacements_user = [{
+        pattern: '/*[[bg-choice]]*/ url(https://stylishthemes.github.io/GitHub-Dark/images/backgrounds/bg-tile1.png)',
+        replacement: '/*[[bg-choice]]*/'
+    },{
+        pattern: /\/\*\[\[base-color\]\]\*\/ #4183C4/g,
+        replacement: '/*[[base-color]]*/'
+    },{
+        pattern: /\/\*\[\[tab-size\]\]\*\/ 4/g,
+        replacement: '/*[[tab-size]]*/'
+    },{
+        // remove default syntax theme
+        pattern: /\s+\/\* GitHub-Dark syntax highlighting(.*(\n|\r))+}$/m,
+        replacement: '\n}'
+    }];
+
+    // for a chrome build, remove the @-moz-document wrapper for easier copy & pasting into the editor
+    if (config.chrome) {
+        config.replacements.push({
+            pattern: /@-moz-document regexp\((.*)\) \{(\n|\r)+/,
             replacement: ''
-        },{
-            pattern: '/*[[pygments-theme]]*/',
-            // add selected theme + closing bracket (if not chrome)
-            replacement: config.newTheme
-        }];
-
-        // userstyles.org - remove defaults & leave placeholders
-        config.replacements_user = [{
-            pattern: '/*[[bg-choice]]*/ url(https://stylishthemes.github.io/GitHub-Dark/images/backgrounds/bg-tile1.png)',
-            replacement: '/*[[bg-choice]]*/'
-        },{
-            pattern: /\/\*\[\[base-color\]\]\*\/ #4183C4/g,
-            replacement: '/*[[base-color]]*/'
-        },{
-            pattern: /\/\*\[\[tab-size\]\]\*\/ 4/g,
-            replacement: '/*[[tab-size]]*/'
-        },{
-            // remove default syntax theme
-            pattern: /\s+\/\* GitHub-Dark syntax highlighting(.*(\n|\r))+}$/m,
-            replacement: '\n}'
-        }];
-
-        // for a chrome build, remove the @-moz-document wrapper for easier copy & pasting into the editor
-        if (config.chrome) {
-            config.replacements.push({
-                pattern: /@-moz-document regexp\((.*)\) \{(\n|\r)+/,
-                replacement: ''
-            });
-        }
+        });
     }
 
     grunt.initConfig({
