437ab39d87b63272edba43a973f1a1efefe7d9b1
silverwind <me@silverwind.io>
2016-10-02 18:10:46 +0200

    Meta: simplify lint grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -191,10 +191,7 @@ module.exports = function(grunt) {
       }
     },
     exec: {
-      stylelint: 'npm run stylelint --silent -- github-dark.css',
-      stylelintCodemirror: 'npm run stylelint --silent -- themes/codemirror/*.css',
-      stylelintGitHub: 'npm run stylelint --silent -- themes/github/*.css',
-      stylelintJupyter: 'npm run stylelint --silent -- themes/jupyter/*.css',
+      stylelint: 'npm run stylelint --silent -- github-dark.css themes/**/*.css --color',
       authors: 'bash tools/authors',
       perfectionist: 'npm run perfectionist --silent -- github-dark.css github-dark.css --indentSize 2 --maxAtRuleLength 250'
     },
@@ -321,12 +318,7 @@ module.exports = function(grunt) {
 
   // lint github-dark.css and themes for errors
   grunt.registerTask('lint', 'Lint CSS for style errors', function() {
-    grunt.task.run([
-      'exec:stylelint',
-      'exec:stylelintCodemirror',
-      'exec:stylelintGitHub',
-      'exec:stylelintJupyter'
-    ]);
+    grunt.task.run(['exec:stylelint']);
   });
 
   // regenerate AUTHORS based on commits
