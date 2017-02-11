428a0549df8643fb4646800387f2899273dc6a15
Rob Garrison <wowmotty@gmail.com>
2017-02-11 01:16:04 -0600

    Themes: Minify separately

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,13 @@
 /* global module, console */
+/* jshint esnext:true, unused:true */
 module.exports = function(grunt) {
   'use strict';
 
-  var config, file;
-
-  try {
-    config = grunt.file.readJSON('build.json');
-  } catch (err) {
-    console.info('build.json not found - using defaults');
-    config = {
-      'theme'    : 'Twilight',
+  let config, file,
+    defaults = {
+      'theme'    : 'twilight',
+      'themeCM'  : 'twilight',
+      'themeJP'  : 'twilight',
       'color'    : '#4183C4',
       'font'     : 'Menlo',
       'image'    : 'url(https://raw.githubusercontent.com/StylishThemes/GitHub-Dark/master/images/backgrounds/bg-tile1.png)',
@@ -19,15 +17,34 @@ module.exports = function(grunt) {
       'tab'      : 4,
       'webkit'   : false
     };
+
+  try {
+    config = Object.assign({}, defaults, grunt.file.readJSON('build.json'));
+  } catch (err) {
+    console.info('build.json not found - using defaults');
+    config = defaults;
   }
 
-  function getTheme() {
-    return (config.theme || '').toLowerCase().replace(/\s+/g, '-');
+  function getTheme(name) {
+    // fallback to twilight theme if name is undefined
+    return (name || 'twilight').toLowerCase().replace(/\s+/g, '-');
+  }
+
+  function loadTheme(name, folder) {
+    let data,
+      theme = getTheme(name);
+    if (grunt.file.exists(`themes/${folder}${theme}.min.css`)) {
+      data = `<%= grunt.file.read("themes/${folder}${theme}.min.css") %>`;
+    } else {
+      // fallback to twilight if file doesn't exist
+      data = `<%= grunt.file.read("themes/${folder}twilight.min.css") %>`;
+    }
+    return data;
   }
 
   function getVersion(level) {
-    var semver = require('semver');
-    var version = require('./package.json').version;
+    const semver = require('semver'),
+      version = require('./package.json').version;
     return semver.inc(version, level);
   }
 
@@ -37,7 +54,7 @@ module.exports = function(grunt) {
 
   // modified from http://stackoverflow.com/a/5624139/145346
   function hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? [
       parseInt(result[1], 16),
       parseInt(result[2], 16),
@@ -47,38 +64,53 @@ module.exports = function(grunt) {
 
   // ** set up build options **
   config.sourceFile = 'github-dark.css';
-  file = getTheme();
-  // setting 'ace' to an empty string, or 'default' will leave the default GitHub-base16 theme in place, with a dark background
-  // using theme src files until we can figure out why cssmin is removing 2/3 of the definitions - see #240
-  config.themeFile = file === '' || file === 'default' ? '' : 'themes/' + file + '.min.css';
+
+  // get themes
+  config.themeMain = loadTheme(config.theme, "github/");
+  config.themeCM = loadTheme(config.themeCM, "codemirror/");
+  config.themeJP = loadTheme(config.themeJP, "jupyter/");
+
   // build file name
-  config.buildFile = 'github-dark-' + (file || 'default') + '-' + config.color.replace(/[^\d\w]/g, '') + '.build.min.css';
+  config.buildFile = `github-dark-${getTheme(config.theme)}-` +
+    `${config.color.replace(/[^\d\w]/g, '')}.build.min.css`;
+
   // background options
-  config.bgOptions = config.tiled ?
-    'background-repeat: repeat !important; background-size: auto !important; background-position: left top !important;' :
-    'background-repeat: no-repeat !important; background-size: cover !important; background-position: center top !important;';
-  config.bgAttachment = config.attach.toLowerCase() === 'scroll' ? 'scroll' : 'fixed';
+  config.bgOptions = config.tiled ? `
+    background-repeat: repeat !important;
+    background-size: auto !important;
+    background-position: left top !important;
+    ` :
+    // fit background
+    `
+    background-repeat: no-repeat !important;
+    background-size: cover !important;
+    background-position: center top !important;
+    `;
 
-  config.codeWrapCss = config.codeWrap ?  [
-    '/* GitHub: Enable wrapping of long code lines */',
-    '  body:not(.nowrap) .blob-code-inner,',
-    '  body:not(.nowrap) .markdown-body pre > code,',
-    '  body:not(.nowrap) .markdown-body .highlight > pre {',
-    '    white-space: pre-wrap !important;',
-    '    word-break: break-all !important;',
-    '    overflow-wrap: break-word !important;',
-    '    display: block !important;',
-    '  }',
-    '  body:not(.nowrap) td.blob-code-inner {',
-    '    display: table-cell !important;',
-    '  }'
-  ].join('\n') : '';
+  config.bgAttachment = config.attach.toLowerCase() === 'scroll' ?
+    'scroll' :
+    'fixed';
 
-  // Don't include closing bracket for a chrome build
-  config.newTheme = config.themeFile ? '<%= grunt.file.read("' + config.themeFile + '") %>' : '';
+  config.codeWrapCss = config.codeWrap ? `
+      /* GitHub: Enable wrapping of long code lines */
+      body:not(.nowrap) .blob-code-inner,
+      body:not(.nowrap) .markdown-body pre > code,
+      body:not(.nowrap) .markdown-body .highlight > pre {
+        white-space: pre-wrap !important;
+        word-break: break-all !important;
+        overflow-wrap: break-word !important;
+        display: block !important;
+      }
+      body:not(.nowrap) td.blob-code-inner {
+        display: table-cell !important;
+      }
+    ` : '';
+
+
 
   // get @-moz prefix
-  file = grunt.file.read('github-dark.css').match(/(@-moz-document regexp\((.*)+\) \{(\n|\r)+)/);
+  file = grunt.file.read('github-dark.css')
+    .match(/(@-moz-document regexp\((.*)+\) \{(\n|\r)+)/);
   config.prefix = file && file.length ? file[1].replace(/^\s+|\s+$/g, '') : '';
 
   // custom build
@@ -116,7 +148,15 @@ module.exports = function(grunt) {
   }, {
     // add selected theme
     pattern: '/*[[syntax-theme]]*/',
-    replacement: config.newTheme
+    replacement: config.themeMain
+  }, {
+    // add Codemirror theme
+    pattern: '/*[[syntax-codemirror]]*/',
+    replacement: config.themeCM
+  }, {
+    // add selected theme
+    pattern: '/*[[syntax-jupyter]]*/',
+    replacement: config.themeJP
   }];
 
   // userstyles.org - remove defaults & leave placeholders
@@ -150,6 +190,7 @@ module.exports = function(grunt) {
         files: {'<%= config.buildFile %>' : '<%= config.sourceFile %>'},
         options: {replacements: '<%= config.replacements %>'}
       },
+      // prevent cssmin from removing the AGENT_SHEET comment for Firefox
       mark: {
         files: {'<%= config.buildFile %>' : '<%= config.buildFile %>'},
         options: {
@@ -168,14 +209,29 @@ module.exports = function(grunt) {
           ]
         }
       },
+      // prevent cssmin from removing userstyles.org placeholders
       fix: {
         files: {'<%= config.buildFile %>' : '<%= config.buildFile %>'},
-        options: {replacements: [{pattern: /\;\:\/\*\[\[/gm, replacement: ';/*[['}]}
+        options: {
+          replacements: [{
+            pattern: /\;\:\/\*\[\[/gm,
+            replacement: ';/*[['
+          }]
+        }
       },
+      // fix cleancss issue:
+      // https://github.com/jakubpawlowicz/clean-css/issues/628
+      // *** TODO: remove? ***
       afterCleanCss: {
         files: {'<%= config.buildFile %>' : '<%= config.buildFile %>'},
-        options: {replacements: [{pattern: /__ESCAPED_SOURCE_END_CLEAN_CSS__/g, replacement: ''}]}
+        options: {
+          replacements: [{
+            pattern: /__ESCAPED_SOURCE_END_CLEAN_CSS__/g,
+            replacement: ''
+          }]
+        }
       },
+      // Tweak Perfectionist results
       afterPerfectionist: {
         files: {'<%= config.sourceFile %>' : '<%= config.sourceFile %>'},
         options: {
@@ -187,7 +243,10 @@ module.exports = function(grunt) {
             {pattern: /,\s+\n/gm, replacement: ',\n'},
             // fix unicode-range block
             {pattern: /\n\s{23}/gm, replacement: ''},
-            {pattern: /(-025A9,|-02662,)/gim, replacement: '$&\n                   '}
+            {
+              pattern: /(-025A9,|-02662,)/gim,
+              replacement: '$&\n                   '
+            }
           ]
         }
       },
@@ -215,10 +274,12 @@ module.exports = function(grunt) {
     },
     clean: {
       cssmins: {
-        src: ['themes/*.min.css']
-      },
-      build: {
-        src: ['themes/build/']
+        src: [
+          // don't clean combined theme files (older method) until
+          // we can be assured that everyone has updated GitHub-Dark-Script
+          // 'themes/*.min.css',
+          'themes/**/*.min.css'
+        ]
       }
     },
     exec: {
@@ -239,12 +300,38 @@ module.exports = function(grunt) {
           advanced: false
         }
       },
-      themes: {
+      codemirror: {
         files: [{
           expand : true,
-          cwd : 'themes/build/',
+          cwd : 'themes/src/codemirror/',
           src : '*.css',
-          dest : 'themes/',
+          dest : 'themes/codemirror',
+          ext : '.min.css'
+        }],
+        options: {
+          keepSpecialComments: '*',
+          advanced: false
+        }
+      },
+      github: {
+        files: [{
+          expand : true,
+          cwd : 'themes/src/github/',
+          src : '*.css',
+          dest : 'themes/github',
+          ext : '.min.css'
+        }],
+        options: {
+          keepSpecialComments: '*',
+          advanced: false
+        }
+      },
+      jupyter: {
+        files: [{
+          expand : true,
+          cwd : 'themes/src/jupyter/',
+          src : '*.css',
+          dest : 'themes/jupyter',
           ext : '.min.css'
         }],
         options: {
@@ -274,7 +361,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-exec');
 
   // build custom GitHub-Dark style using build.json settings
-  grunt.registerTask('default', 'Building custom style', function() {
+  grunt.registerTask('default', 'Building custom style', () => {
     config.buildFile = config.buildFile.replace('.min.css', '.css');
     grunt.task.run(['string-replace:inline']);
     if (!(config.chrome || config.webkit)) {
@@ -283,15 +370,16 @@ module.exports = function(grunt) {
   });
 
   // build custom minified GitHub-Dark style
-  grunt.registerTask('minify', 'Building custom minified style', function() {
+  grunt.registerTask('minify', 'Building custom minified style', () => {
     grunt.task.run(['string-replace:inline', 'cssmin:minify']);
     if (!(config.chrome || config.webkit)) {
       grunt.task.run(['wrap']);
     }
   });
 
-  // build userstyle for pasting into https://userstyles.org/styles/37035/github-dark
-  grunt.registerTask('user', 'building userstyles.org file', function() {
+  // build userstyle for pasting into
+  // https://userstyles.org/styles/37035/github-dark
+  grunt.registerTask('user', 'building userstyles.org file', () => {
     config.buildFile = 'github-dark-userstyle.build.css';
     config.replacements = config.replacements_user;
     grunt.task.run([
@@ -299,7 +387,7 @@ module.exports = function(grunt) {
       'wrap'
     ]);
   });
-  grunt.registerTask('usermin', 'building userstyles.org file', function() {
+  grunt.registerTask('usermin', 'building userstyles.org file', () => {
     config.buildFile = 'github-dark-userstyle.build.css';
     config.replacements = config.replacements_user;
     grunt.task.run([
@@ -314,41 +402,16 @@ module.exports = function(grunt) {
   });
 
   // build custom minified GitHub-Dark style
-  grunt.registerTask('themes', 'Rebuild minified theme files', function() {
-    grunt.task.run(['clean:cssmins']);
-    var fallbacks = {
-      codemirror: grunt.file.read('themes/codemirror/twilight.css'),
-      jupyter: grunt.file.read('themes/jupyter/twilight.css')
-    };
-    grunt.file.mkdir('themes/build');
-    grunt.file.expand({
-      filter: 'isFile',
-      cwd: 'themes/github/',
-    }, [
-      '*.css',
-      '!_template.css'
-    ]).forEach(function(name) {
-      // concat similar named theme files; use fallback if it doesn't exist
-      var cur = grunt.file.read('themes/github/' + name);
-      console.log(name);
-      if (cur) {
-        cur += grunt.file.exists('themes/codemirror/' + name) ?
-          grunt.file.read('themes/codemirror/' + name) : fallbacks.codemirror;
-        cur += grunt.file.exists('themes/jupyter/' + name) ?
-          grunt.file.read('themes/jupyter/' + name) : fallbacks.jupyter;
-        grunt.file.write('themes/build/' + name, cur);
-      } else {
-        grunt.log.error('Unable to read ' + cur);
-      }
-    });
-    // compress & clean
+  grunt.registerTask('themes', 'Rebuild minified theme files', () => {
     grunt.task.run([
-      'cssmin:themes',
-      'clean:build'
+      'clean:cssmins',
+      'cssmin:codemirror',
+      'cssmin:github',
+      'cssmin:jupyter'
     ]);
   });
 
-  grunt.registerTask('clean', 'Perfectionist cleanup', function() {
+  grunt.registerTask('clean', 'Perfectionist cleanup', () => {
     grunt.task.run([
       'exec:stylelint', // check linting first
       'exec:perfectionist',
@@ -357,28 +420,28 @@ module.exports = function(grunt) {
   });
 
   // lint github-dark.css and themes for errors
-  grunt.registerTask('lint', 'Lint CSS for style errors', function() {
+  grunt.registerTask('lint', 'Lint CSS for style errors', () => {
     grunt.task.run(['exec:stylelint']);
   });
 
   // regenerate AUTHORS based on commits
-  grunt.registerTask('authors', 'Regenerate AUTHORS', function() {
+  grunt.registerTask('authors', 'Regenerate AUTHORS', () => {
     grunt.task.run(['exec:authors']);
   });
 
   // minify all PNG and SVG images
-  grunt.registerTask('imagemin', 'Minify all PNG and SVG images', function() {
+  grunt.registerTask('imagemin', 'Minify all PNG and SVG images', () => {
     grunt.task.run(['exec:imagemin']);
   });
 
   // version bump tasks
-  grunt.registerTask('patch', 'Bump patch version', function() {
+  grunt.registerTask('patch', 'Bump patch version', () => {
     grunt.task.run(['string-replace:patch', 'exec:add', 'exec:patch', 'user']);
   });
-  grunt.registerTask('minor', 'Bump minor version', function() {
+  grunt.registerTask('minor', 'Bump minor version', () => {
     grunt.task.run(['string-replace:minor', 'exec:add', 'exec:minor', 'user']);
   });
-  grunt.registerTask('major', 'Bump major version', function() {
+  grunt.registerTask('major', 'Bump major version', () => {
     grunt.task.run(['string-replace:major', 'exec:add', 'exec:major', 'user']);
   });
 
