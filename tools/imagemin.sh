7d66bb2656748285c6d6c7a17389a8885a1b9bac
silverwind <me@silverwind.io>
2017-02-12 12:19:36 +0100

    Meta: Update screenshot and imagemin script

--- tools/imagemin.sh
+++ tools/imagemin.sh
@@ -1,40 +1,33 @@
-#/bin/bash
+#/usr/bin/env bash
 
-shopt -s globstar
+if [ $# -lt 1 ]; then
+    echo "Usage: $(basename $0) FILE .. FILE"
+    exit 1
+fi
+
+# shopt -s globstar
 shopt -s nocaseglob
 
 repo="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd)"
 
 err=false
-if ! hash pngcrush 2>/dev/null; then
-  echo "pngcrush not found. Install it through: npm i -g pngcrush-bin"
-  err=true
-fi
-if ! hash zopflipng 2>/dev/null; then
-  echo "zopflipng not found. Install it through: npm i -g zopflipng-bin"
-  err=true
-fi
-if ! hash svgo 2>/dev/null; then
-  echo "svgo not found. Install it through: npm i -g svgo"
-  err=true
-fi
-
+if ! hash pngcrush 2>/dev/null; then err=true; fi
+if ! hash zopflipng 2>/dev/null; then err=true; fi
+if ! hash svgo 2>/dev/null; then err=true; fi
 if "$err"; then
-  exit 1
+  echo "Missing dependencies. Install through: npm install -g pngcrush-bin zopflipng-bin svgo"
 fi
 
-# optimize PNGs
-for f in $repo/**/*.png; do
-  f=$(realpath --relative-to=$PWD "$f")
-  pngcrush -reduce -brute -ow "$f"
-done
-for f in $repo/**/*.png; do
-  f=$(realpath --relative-to=$PWD "$f")
-  zopflipng -y -m --splitting=3 "$f" "$f"
-done
+set -x
 
-# optimize SVGs
-for f in $repo/**/*.svg; do
+for f in $@;do
   f=$(realpath --relative-to=$PWD "$f")
-  svgo --multipass -i "$f" -o "$f"
+  if echo "$f" | egrep -iq ".png$"; then
+    pngcrush -reduce -brute -ow "$f"
+    zopflipng -y -m --splitting=3 "$f" "$f"
+  elif echo "$@" | egrep -iq ".svg$"; then
+    svgo --multipass -i "$f" -o "$f"
+  else
+    echo "no"
+  fi
 done
