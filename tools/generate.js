32a0dbd16e5dc6ea0f0eff255ac4e7794cf2c421
silverwind <me@silverwind.io>
2019-03-03 14:07:16 +0100

    generate: ignore .bg-gray

--- tools/generate.js
+++ tools/generate.js
@@ -210,9 +210,11 @@ const ignoreSelectors = [
   /^.Popover-message:before$/,
   /^.Popover-message:after$/,
   /^h[1-6] a$/, // weird styles from help.github.com
-  /\.bg-white/,
-  /\.CircleBadge/,
+  /^\.bg-white$/,
+  /^\.bg-gray$/,
+  /^\.CircleBadge$/,
   /^table$/,
+
 ];
 
 // list of regexes matching selectors that shouldn't be merged with other
