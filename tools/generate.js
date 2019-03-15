a219bc1e0534195a25f1c01a5e8f47f7468bcbf9
silverwind <me@silverwind.io>
2019-03-15 15:34:07 +0100

    comment label fixes - fixes #884

--- tools/generate.js
+++ tools/generate.js
@@ -58,6 +58,8 @@ const mappings = {
   "border-color: #e1e4e8": "border-color: #343434",
   "border-color: #d1d5da": "border-color: #484848",
 
+  "border: 1px solid rgba(27,31,35,.1)": "border-color: rgba(200,200,200,.1)",
+
   "border: 1px solid rgba(27,31,35,.15)": "border-color: rgba(200,200,200,.15)",
   "border-top-color: rgba(27,31,35,.15)": "border-top-color: rgba(200,200,200,.15)",
   "border-bottom-color: rgba(27,31,35,.15)": "border-bottom-color: rgba(200,200,200,.15)",
