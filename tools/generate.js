235837d7d6562e62e24c83cd2f673ead47247522
the-j0k3r <31389848+the-j0k3r@users.noreply.github.com>
2019-04-07 15:17:39 +0100

    generator: remove topic-tag-outline re: https://github.com/StylishThemes/GitHub-Dark/issues/907#issuecomment-480593830

--- tools/generate.js
+++ tools/generate.js
@@ -149,7 +149,6 @@ const mappings = {
 
   "box-shadow: 0 0 0 .2em rgba(203,36,49,.4)": "box-shadow: 0 0 0 .2em rgba(255,68,68,.4)",
 
-  "box-shadow: inset 0 0 0 1px #c8e1ff": "box-shadow: inset 0 0 0 1px #6d7c9c",
   "box-shadow: 0 1px 5px rgba(27,31,35,.15)": "box-shadow: 0 1px 5px #000",
 
   "box-shadow: inset 0 0 0 1px #e1e4e8,0 2px 4px rgba(0,0,0,.15)": "box-shadow: inset 0 0 0 1px #555",
