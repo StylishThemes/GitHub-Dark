27afabda47f9f424effd3edcb4877db47dff2397
the-j0k3r <31389848+the-j0k3r@users.noreply.github.com>
2019-06-18 06:59:01 +0100

    generator: add MarketplaceAnnouncement linear bg to blue
    this styles the Pull Panda announcement at 
    https://github.com/marketplace

--- tools/generate.js
+++ tools/generate.js
@@ -168,6 +168,7 @@ let mappings = {
   "$border: #c0d3eb": "#224466",
   "$background: #0361cc": "#182030",
   "$background: linear-gradient(-180deg,#0679fc,#0361cc 90%)": "linear-gradient(to bottom,#283040,#182030)",
+  "$background: linear-gradient(90deg,#257bf9,#2426ca)": "linear-gradient(90deg,#283040,#182030)",
 
   // blue (base-color)
   "color: #327fc7": "color: /*[[base-color]]*/ #4f8cc9",
