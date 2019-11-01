StylishThemes/GitHub-Dark

commit ee796285ef388707f82d09f1200f7c0f88bc8e4f

fix .clone-url-button > .clone-url-link
* remove .clone-url-button > .clone-url-link from important rule
* add bg image and border to generator

see commit comment for gif before and after

tools/generate.js
@@ -196,6 +196,8 @@ let mappings = {
   "$border: #2188ff": "/*[[base-color]]*/ #4f8cc9",
   "$background: linear-gradient(#54a3ff,#006eed)": "linear-gradient(/*[[base-color]]*/ #4f8cc9, /*[[base-color]]*/ #4f8cc9)", // notification icon
   "color: #cce888": "color: /*[[base-color]]*/ #4f8cc9", // https://github.com/StylishThemes/GitHub-Dark/issues/954
+  "background-image: linear-gradient(#599bcd,#3072b3)": "background-image: linear-gradient(/*[[base-color]]*/ #4f8cc9, /*[[base-color]]*/ #4f8cc9)",
+  "$border: #2a65a0": "/*[[base-color]]*/ #4f8cc9",
 
   // red
   "color: #cb2431": "color: #f44",

github-dark.css
@@ -4136,6 +4136,16 @@
   .contrib-square {
     color: /*[[base-color]]*/ #4f8cc9;
   }
+  /* auto-generated rule for "background-image: linear-gradient(#599bcd,#3072b3)" */
+  .clone-url-button > .clone-url-link:active,
+  .clone-url-button > .clone-url-link:hover {
+    background-image: linear-gradient(/*[[base-color]]*/ #4f8cc9, /*[[base-color]]*/ #4f8cc9);
+  }
+  /* auto-generated rule for "border-color: #2a65a0" */
+  .clone-url-button > .clone-url-link:active,
+  .clone-url-button > .clone-url-link:hover {
+    border-color: /*[[base-color]]*/ #4f8cc9;
+  }
   /* auto-generated rule for "color: #cb2431" */
   .btn-danger, .form-group dd h4.is-error, .form-group.required dt label:after,
   .form-group.errored label, .status-indicator-failed .octicon-x,
@@ -7086,10 +7096,13 @@
   .cheatsheet h2, .section a.section-head, .list-heading, .dashboard-tabs li a,
   a.function-button, .add-pill-form, .gist .gist-file .gist-meta,
   ul.comparison-list > li.title, button.button, a.button, span.button,
-  button.minibutton, a.minibutton, span.minibutton,
-  .clone-url-button > .clone-url-link {
+  button.minibutton, a.minibutton, span.minibutton {
     background: linear-gradient(#202020, #181818) !important;
     border-color: #383838 !important;
+  }
+  .clone-url-button > .clone-url-link {
+    background: linear-gradient(#202020, #181818);
+    border-color: #383838;
   }
   .search-form button, .issues-list-actions, a.gollum-minibutton,
   a.gollum-minibutton:visited {

commit 3aa02e4c96970aa796aa91cfaf10f3639bf3a3fe

generator: use $background closes #1004
original fixes: ee79628 and e608dab

tools/generate.js
@@ -42,8 +42,7 @@ let mappings = {
   "$background: hsla(0,0%,100%,.125)": "hsla(0,0%,100%,.05)",
   "$background: hsla(0,0%,100%,.175)": "hsla(0,0%,100%,.1)",
   "$background: linear-gradient(180deg,rgba(242,248,254,0),rgba(242,248,254,.47))": "linear-gradient(180deg,#181818,#181818)",
-
-  "background-image: linear-gradient(#fafafa,#eaeaea)": "background-image: linear-gradient(#202020, #181818)",
+  "$background: linear-gradient(#fafafa,#eaeaea)": "linear-gradient(#202020, #181818)",
 
   // ==========================================================================
   // Border
@@ -199,7 +198,7 @@ let mappings = {
   "$border: #2188ff": "/*[[base-color]]*/ #4f8cc9",
   "$background: linear-gradient(#54a3ff,#006eed)": "linear-gradient(/*[[base-color]]*/ #4f8cc9, /*[[base-color]]*/ #4f8cc9)", // notification icon
   "color: #cce888": "color: /*[[base-color]]*/ #4f8cc9", // https://github.com/StylishThemes/GitHub-Dark/issues/954
-  "background-image: linear-gradient(#599bcd,#3072b3)": "background-image: linear-gradient(/*[[base-color]]*/ #4f8cc9, /*[[base-color]]*/ #4f8cc9)",
+  "$background: linear-gradient(#599bcd,#3072b3)": "linear-gradient(/*[[base-color]]*/ #4f8cc9, /*[[base-color]]*/ #4f8cc9)",
   "$border: #2a65a0": "/*[[base-color]]*/ #4f8cc9",
 
   // red