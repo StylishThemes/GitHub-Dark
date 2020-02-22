9e5a89b3e1f0329cdf5c9d21a4bd67d4380f80f9
silverwind <me@silverwind.io>
2020-02-22 19:14:06 +0100

    switch to fetch-css

--- tools/generate.js
+++ tools/generate.js
@@ -1,18 +1,11 @@
 #!/usr/bin/env node
 "use strict";
 
-const acorn = require("acorn");
-const css = require("css");
-const doFetch = require("make-fetch-happen");
-const parse5 = require("parse5");
-const path = require("path");
+const fetchCss = require("fetch-css");
 const remapCss = require("remap-css");
-const unzipper = require("unzipper");
-const urlToolkit = require("url-toolkit");
+const {join} = require("path");
 const {readFile, writeFile} = require("fs").promises;
 
-// This list maps old declarations to new ones. Ordering is significant.
-// $ indicates a special value that will generate a set of rules.
 const mappings = {
   // ==========================================================================
   // Background
@@ -326,7 +319,7 @@ const mappings = {
 // start with one of the selectors in `match`. If `url` ends with .css, will
 // directly load that stylesheet. `crx` refers so a Chrome extension id and can
 // be used in place of `url`.
-let sources = [
+const sources = [
   {url: "https://github.com"},
   {url: "https://gist.github.com"},
   {url: "https://help.github.com"},
@@ -351,7 +344,7 @@ let sources = [
     url: "https://github.com/StylishThemes/GitHub-Dark",
     prefix: `body[class="page-responsive"]`,
     match: ["body", ".page-responsive"],
-    opts: {headers: {"User-Agent": "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Mobile Safari/537.36"}},
+    fetchOpts: {headers: {"User-Agent": "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Mobile Safari/537.36"}},
   },
   {
     url: [
@@ -404,196 +397,23 @@ const ignoreSelectors = [
 ];
 
 const replaceRe = /.*begin remap-css[\s\S]+end remap-css.*/gm;
-const cssFile = path.join(__dirname, "..", "github-dark.css");
-
-async function fetch(...args) {
-  if (process.argv.includes("-v")) console.info(`fetch ${args[0]}`);
-  const result = await doFetch(...args);
-  if (process.argv.includes("-v")) console.info(`done ${args[0]}`);
-  return result;
-}
-
-async function writeOutput(generatedCss) {
-  const css = await readFile(cssFile, "utf8");
-  await writeFile(cssFile, css.replace(replaceRe, generatedCss));
-}
-
-async function extract(res) {
-  const styleUrls = [];
-  const styleTags = [];
-  const html = await res.text();
-
-  for (const href of extractStyleHrefs(html)) {
-    styleUrls.push(urlToolkit.buildAbsoluteURL(res.url, href));
-  }
-  for (const style of extractStyleTags(html)) {
-    styleTags.push(style);
-  }
-
-  return [styleUrls, styleTags];
-}
-
-function extractStyleHrefs(html) {
-  return (html.match(/<link.+?>/g) || []).map(link => {
-    const attrs = {};
-    parse5.parseFragment(link).childNodes[0].attrs.forEach(attr => {
-      attrs[attr.name] = attr.value;
-    });
-    if (attrs.rel === "stylesheet" && attrs.href) {
-      return attrs.href;
-    }
-  }).filter(link => !!link);
-}
-
-function extractStyleTags(html) {
-  const matches = Array.from((html || "").matchAll(/<style.*?>([\s\S]*?)<\/style>/g) || []);
-  return matches.map(match => match[1]).map(css => css.trim()).filter(css => !!css);
-}
+const cssFile = join(__dirname, "..", "github-dark.css");
 
 function exit(err) {
   if (err) console.error(err);
   process.exit(err ? 1 : 0);
 }
 
-function isValidCSS(string) {
-  try {
-    const result = css.parse(string);
-    if (result && result.type === "stylesheet" && result.stylesheet && Array.isArray(result.stylesheet.rules) && result.stylesheet.rules.length > 1) {
-      return true;
-    }
-  } catch (err) {}
-  return false;
-}
-
-// obtain the latest chrome version in major.minor format
-async function chromeVersion() {
-  const res = await fetch(`https://chromedriver.storage.googleapis.com/LATEST_RELEASE`);
-  if (!res.ok) throw new Error(res.statusText);
-
-  const text = await res.text();
-  const [version] = text.match(/[0-9]+\.[0-9]+/) || [];
-
-  if (!version) throw new Error(`Unable to match version in response text '${text}'`);
-  return version;
-}
-
-async function extensionCss(source, version) {
-  const id = source.crx;
-  let css = "";
-
-  const res = await fetch(`https://clients2.google.com/service/update2/crx?response=redirect&prodversion=${version}&x=id%3D${id}%26installsource%3Dondemand%26uc`);
-  if (!res.ok) throw new Error(res.statusText);
-
-  const buffer = await res.buffer();
-  const dir = await unzipper.Open.buffer(buffer, {crx: true});
-  const files = {};
-
-  for (const file of dir.files) {
-    files[file.path] = file;
-  }
-
-  if (!files["manifest.json"]) {
-    throw new Error(`manifest.json not found in chrome extension ${id}`);
-  }
-
-  let cssFiles = [];
-  let jsFiles = [];
-
-  if (!source.contentScriptsOnly) {
-    for (const path of Object.keys(files)) {
-      if (path.endsWith(".css")) cssFiles.push(path);
-      if (path.endsWith(".js")) jsFiles.push(path);
-    }
-  }
-
-  const manifest = JSON.parse(String(await files["manifest.json"].buffer()));
-
-  for (const {css, js} of manifest.content_scripts || []) {
-    if (Array.isArray(css) && css.length) cssFiles.push(...css);
-    if (Array.isArray(js) && js.length) jsFiles.push(...js);
-  }
-
-  cssFiles = Array.from(new Set(cssFiles));
-  jsFiles = Array.from(new Set(jsFiles));
-
-  for (const file of cssFiles) {
-    css += `${await files[file].buffer()}\n`;
-  }
-
-  for (const file of jsFiles) {
-    acorn.parse(String(await files[file].buffer()), {onToken: async token => {
-      if (token.type.label === "string") {
-        const str = token.value.trim()
-          .replace(/\n/gm, "")
-          .replace(/^\);}/, ""); // this is probably not universal to webpack's css-in-js strings
-        if (str.length > 25 && isValidCSS(str)) { // hackish treshold to ignore short strings that may be valid CSS
-          css += `${str}\n`;
-        }
-      }
-    }});
-  }
-
-  for (const file of source.files || []) {
-    if (Object.keys(files).includes(file)) {
-      css += `${await files[file].buffer()}\n`;
-    }
-  }
-
-  return css;
-}
-
 async function main() {
-  const expandedSources = [];
-  for (const source of sources) {
-    if ("url" in source && Array.isArray(source.url)) {
-      for (const url of source.url) {
-        expandedSources.push({...source, url});
-      }
-    } else {
-      expandedSources.push(source);
-    }
-  }
-  sources = expandedSources;
-
-  const sourceResponses = await Promise.all(sources.map(source => {
-    if (!source.url) return null;
-    return source.url.endsWith(".css") ? null : fetch(source.url, source.opts);
-  }));
-
-  for (const [index, response] of Object.entries(sourceResponses)) {
-    const source = sources[index];
-    if (response) {
-      const [styleUrls, styleTags] = await extract(response);
-      source.styles = styleUrls;
-      source.styleTags = styleTags;
-    } else if (source.url) {
-      source.styles = [source.url];
-    }
-  }
-
-  const cssResponses = await Promise.all(sources.map(source => {
-    if (!source.url) return null;
-    return Promise.all(source.styles.map(url => fetch(url).then(res => res.text())));
-  }));
-
-  const version = await chromeVersion();
-
-  for (const [index, responses] of Object.entries(cssResponses)) {
-    if (sources[index].crx) {
-      sources[index].css = await extensionCss(sources[index], version);
-    } else {
-      sources[index].css = responses.join("\n");
-      if (sources[index].styleTags.length) {
-        sources[index].css += `\n${sources[index].styleTags.join("\n")}`;
-      }
-    }
-  }
-
-  await writeOutput(await remapCss(sources, mappings, {
+  const generatedCss = await remapCss(await fetchCss(sources), mappings, {
     ignoreSelectors,
-    indent: 2,
+    indentCss: 2,
     lineLength: 76,
-  }));
+    comments: true,
+  });
+
+  const css = await readFile(cssFile, "utf8");
+  await writeFile(cssFile, css.replace(replaceRe, generatedCss));
 }
 
 main().then(exit).catch(exit);
