9bb1ec1924f05d0b232657cd9a5ad64eff2c7ea3
silverwind <me@silverwind.io>
2020-05-27 02:58:12 +0200

    fix buttons active background

--- tools/generate.js
+++ tools/generate.js
@@ -311,7 +311,7 @@ const mappings = {
   "$border: #dcffe4": "#002800",
   "$background: linear-gradient(-180deg, #34d058, #28a745 90%)": "linear-gradient(180deg, #373 0%, #060 90%)", // .btn-primary
   "$background: linear-gradient(-180deg, #2fcb53, #269f42 90%)": "linear-gradient(180deg, #484 5%, #171 90%)", // .btn-primary :hover
-  "background-color: #279f43": "background: linear-gradient(180deg, #595 0%, #283 100%)", // .btn-primary:active
+  "$background: #279f43": "#484", // .btn-primary:active
   "background-color: #94d3a2": "background: #040", // .btn-primary.disabled
   "background: linear-gradient(#34d058, #28a745)": "background: linear-gradient(#34d058, #28a745)", // refined github
   "box-shadow: 0 0 0 .2em rgba(52,208,88,.4)": "box-shadow: 0 0 0 .2em #3737",
