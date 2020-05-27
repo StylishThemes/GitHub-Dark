beb7a85fc6560d5513283160deed10fc321aea2f
silverwind <me@silverwind.io>
2020-05-28 00:30:37 +0200

    remove manual pagination styling

--- tools/generate.js
+++ tools/generate.js
@@ -24,7 +24,7 @@ const mappings = {
   "$background: #f5f5f5": "#202020", // gitako
   "$background: #f4f5f5": "#242424", // zenhub
   "$background: #f4f4f4": "#242424",
-  "$background: #eff3f6": "#343434",
+  "$background: #eff3f6": "#242424",
   "$background: #eaecef": "#343434",
   "$background: #e9ecef": "#404040",
   "$background: #e9e9e9": "#343434", // zenhub
