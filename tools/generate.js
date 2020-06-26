1ccd3d195095c9f580d7c7b9169a00daa03d8c0d
the-j0k3r <31389848+the-j0k3r@users.noreply.github.com>
2020-06-26 08:45:02 +0100

    gen: move #264665 to base-color
    this already uses base color when not disabled lloks odd to use fixed blue colors

--- tools/generate.js
+++ tools/generate.js
@@ -39,9 +39,9 @@ const mappings = {
   "$color: #0366d6": "/*[[base-color]]*/ #4f8cc9", // gitako
   "$color: #0366d6d0": "/*[[base-color]]*/ #4f8cc9", // gitako
   "$color: #3b5998": "/*[[base-color]]*/ #4f8cc9",
+  "$color: rgba(3,102,214,.5)": "/*[[base-color]]*/ #4f8cc9",
 
   // blue
-  "$color: rgba(3,102,214,.5)": "#264665",
   "$color: rgba(4,66,137,.6)": "#4f8cc9", // .flash .octicon
   "$color: #eaf5ff": "#182030", // .full-commit
   "$color: #0679fc": "#4f8cc9",
