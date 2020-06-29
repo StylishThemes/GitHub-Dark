84f0707320be20e44578c372297ffe89560a5c69
silverwind <me@silverwind.io>
2020-06-29 17:46:41 +0200

    fix medium and extra-large box shadow - fixes #1155

--- tools/generate.js
+++ tools/generate.js
@@ -19,6 +19,8 @@ const mappings = {
   "$color: rgba(209,213,218,0)": "transparent",
   "$color: rgba(209,213,218,.5)": "rgba(90,90,90,.5)",
   "$color: rgba(149,157,165,.2)": "rgba(0,0,0,.2)", // modal backdrop
+  "$color: rgba(149,157,165,.3)": "rgba(0,0,0,.3)", // .box-shadow-extra-large
+  "$color: rgba(149,157,165,.15)": "rgba(0,0,0,.15)", // .box-shadow-medium
   "$color: rgba(0,0,0,0.2)": "rgba(255,255,255,0.7)",
   "$color: hsla(0,0%,100%,.8)": "hsla(0,0%,100%,.8)",
   "$color: hsla(0,0%,100%,.6)": "hsla(0,0%,0%,.6)",
