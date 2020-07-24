a11f078c536d1ebe765656ed38636d9ff930017a
the-j0k3r <31389848+the-j0k3r@users.noreply.github.com>
2020-07-24 14:01:57 +0100

    ignore .CircleBadge--github::after
    its better than keeping broken gen stuff taking space
    
    see https://github.com/StylishThemes/GitHub-Dark/commit/c3d390b086438afb1d22b7866d7435098cf5d931

--- src/gen/ignores.js
+++ src/gen/ignores.js
@@ -4,6 +4,7 @@ module.exports.ignores = [
   /\.pl-/, // GitHub Pretty Lights Syntax highlighter
   /^h[1-6] a$/, // conflicting styles from docs.github.com
   /^\.CircleBadge$/,
+  /^\.CircleBadge--github::after$/,
   /^table$/,
   /^.markdown-body del$/, // this in not main page style
   /:(before|after).+/, // invalid pseudo-elements, they must come last in a chain of
