10ee2eb67e6537a74d9e102aff6290acd9d077bb
silverwind <me@silverwind.io>
2020-07-26 13:44:12 +0200

    remove obsolete ignores

--- src/gen/ignores.js
+++ src/gen/ignores.js
@@ -2,11 +2,8 @@ module.exports.ignores = [
   /\.CodeMirror/,
   /\.cm-/, // CodeMirror
   /\.pl-/, // GitHub Pretty Lights Syntax highlighter
-  /^h[1-6] a$/, // conflicting styles from docs.github.com
   /^\.CircleBadge$/,
   /^\.CircleBadge--github::after$/,
-  /^table$/,
-  /^.markdown-body del$/, // this in not main page style
   /:(before|after).+/, // invalid pseudo-elements, they must come last in a chain of
   /:not\(li\.moved\)/, // invalid :not content (not a simple selector)
 ];
