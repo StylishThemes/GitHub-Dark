4c67124307b6003ce88cf149c79b7fd2923cac97
silverwind <me@silverwind.io>
2021-03-10 15:42:57 +0100

    restore module path

--- tools/build.js
+++ tools/build.js
@@ -2,7 +2,7 @@
 "use strict";
 
 const esc = require("escape-string-regexp");
-const fetchCss = require("../../fetch-css");
+const fetchCss = require("fetch-css");
 const remapCss = require("remap-css");
 const {readFile} = require("fs").promises;
 const {resolve, basename} = require("path");
